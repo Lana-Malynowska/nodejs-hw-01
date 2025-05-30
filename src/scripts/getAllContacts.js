5c6bb8f5c01d1f9341254d2b8c338cd31c998d14
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-05-30 13:59:02 +0300

    add getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,18 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const existingContacts = await readContacts();
+
+    if (!Array.isArray(existingContacts)) {
+      throw new TypeError('existingContacts is not an array');
+    }
+
+    return existingContacts;
+  } catch (error) {
+    console.error('Error occurs:', error);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
